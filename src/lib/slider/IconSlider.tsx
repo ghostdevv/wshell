00e371dd7ccb984451e005e3fa50ad7af5c7e08a
Willow (GHOST) <git@willow.sh>
2025-09-06 23:11:33 +0100

    feat: add icon slider cmp

---
+++ src/lib/slider/IconSlider.tsx
@@ -0,0 +1,29 @@
+import type { Accessor } from 'gnim';
+import { Gtk } from 'ags/gtk4';
+
+interface Props {
+	icon: string | Accessor<string>;
+	value: number | Accessor<number>;
+	onChangeValue: (value: number) => void;
+}
+
+export function IconSlider(props: Props) {
+	return (
+		<box class="icon-slider">
+			<overlay>
+				<label
+					$type="overlay"
+					halign={Gtk.Align.START}
+					label={props.icon}
+				/>
+
+				<slider
+					min={0}
+					max={1}
+					value={props.value}
+					onChangeValue={({ value }) => props.onChangeValue(value)}
+				/>
+			</overlay>
+		</box>
+	);
+}
