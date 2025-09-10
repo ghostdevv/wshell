bfd996a5f5d9cbf07f6a1aa3c5b821253a204b71
Willow (GHOST) <git@willow.sh>
2025-09-10 06:10:34 +0100

    fix: date-fns usage in clocks

--- src/bar/Clock.tsx
+++ src/bar/Clock.tsx
@@ -2,9 +2,10 @@ import CenterDropdown from '../center-dropdown/CenterDropdown';
 import { handleDropdownWindow } from '../lib/dropdown/utils';
 import { createPoll } from 'ags/time';
 import { Gdk, Gtk } from 'ags/gtk4';
+import { format } from 'date-fns';
 
 export function Clock(props: { monitor: Gdk.Monitor }) {
-	const time = createPoll('', 500, 'date +%H:%M:%S');
+	const time = createPoll('', 500, () => format(new Date(), 'HH:mm:ss'));
 
 	const centerDropdownWindow = handleDropdownWindow(
 		CenterDropdown({
--- src/center-dropdown/CenterDropdown.tsx
+++ src/center-dropdown/CenterDropdown.tsx
@@ -1,20 +1,17 @@
 import { DropdownWindow } from '../lib/dropdown/DropdownWindow';
 import { Astal, Gtk, Gdk } from 'ags/gtk4';
-import { interval } from 'ags/time';
-import { createState } from 'gnim';
+import { createPoll } from 'ags/time';
 import { format } from 'date-fns';
 
 export default function CenterDropdown(props: { monitor: Gdk.Monitor }) {
-	const { TOP } = Astal.WindowAnchor;
-	const [now, setNow] = createState(new Date());
-
-	interval(500, () => setNow(new Date()));
+	const date = createPoll('', 1000, () => format(new Date(), 'do MMMM yyyy'));
+	const time = createPoll('', 500, () => format(new Date(), 'HH:mm:ss'));
 
 	return (
 		<DropdownWindow
 			name="center-dropdown"
 			monitor={props.monitor}
-			anchor={TOP}
+			anchor={Astal.WindowAnchor.TOP}
 		>
 			<box orientation={Gtk.Orientation.VERTICAL} spacing={4}>
 				<box
@@ -23,14 +20,14 @@ export default function CenterDropdown(props: { monitor: Gdk.Monitor }) {
 					spacing={4}
 				>
 					<label
+						label={date}
 						halign={Gtk.Align.START}
-						label={now.as((now) => format(now, 'do MMMM yyyy'))}
 						css="font-size: 22px;"
 					/>
 
 					<label
+						label={time}
 						halign={Gtk.Align.START}
-						label={now.as((now) => format(now, 'HH:mm:ss'))}
 						css="font-size: 18px;"
 					/>
 				</box>
