a270fcb10c23e65b37176e907cacea40933de30c
Willow (GHOST) <git@willow.sh>
2025-09-26 21:53:35 +0100

    fix: close dropdown window when their parents are closed

--- src/lib/dropdown/utils.ts
+++ src/lib/dropdown/utils.ts
@@ -1,6 +1,11 @@
 import type { Astal } from 'ags/gtk4';
+import { onCleanup } from 'gnim';
 
 export function handleDropdownWindow(window: Astal.Window) {
+	onCleanup(() => {
+		window.close();
+	});
+
 	return {
 		toggle() {
 			if (!window.visible) {
